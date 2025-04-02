5c2eefd71680b2211435155b2d0b2b3487356d0c
Michael Figueiredo <michaelfigueiredo743@gmail.com>
2025-04-02 08:56:33 -0700

    added gitignore to hide api_key as per notification of leak on gitguard via github

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from api_key import API_KEY
 
 # Build out the API and get a response
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+MAP_URL = "https://www.openstreetmap.org/export/embed.html?bbox=lon1,lat1,lon2,lat2&layer=mapnik"
 
 def get_weather(city):
     params = {"q":city, "appid": API_KEY, "units": "metric"} # Use metric for Celsius
@@ -43,10 +44,32 @@ def main(page: ft.Page):
 
     results_text = ft.Text("",size=18,weight=ft.FontWeight.BOLD,color=ft.colors.WHITE) # Text widget to show results
  
+    # Call and get the weather data
+    def search_weather(e):
+        city=city_input.value.strip() # Get the value from the TextField and strip any extra spaces
+        if not city:
+            results_text.value = "Please enter a city name..."
+            page.update()
+            return
+        weather_data = get_weather(city)
+
+        if weather_data:
+            results_text.value = f"City: {weather_data['city']}\nTemperature: {weather_data["temp"]}°C\nHumidity: {weather_data['humidity']}%\nWeather: {weather_data["weather"]}"
+
+        else:
+            results_text.value = "City not found. Try again..."
+
+        page.update() # Update the page to reflect the changes in results_text
+
+        
+
+
+
     # Search Button
 
     search_button = ft.ElevatedButton(
         "Search",
+        on_click=search_weather, # Bind the search_weather function to the button click
         bgcolor=ft.colors.BLUE_500, # Button color
         color=ft.colors.WHITE, # Text color
         style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10), padding=10)
